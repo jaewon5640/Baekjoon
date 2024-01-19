fe48efd347478a5d709e78bf2994c1152474bcd7
jaewon5640 <47936023+jaewon5640@users.noreply.github.com>
2024-01-19 16:45:49 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%s","|\\_/|\n");
+	printf("%s","|q p|   /}\n");
+	printf("%s","( 0 )\"\"\"\\\n");
+	printf("%s","|\"^\"`    |\n");
+	printf("%s", "||_/=\\\\__|\n");
+}
